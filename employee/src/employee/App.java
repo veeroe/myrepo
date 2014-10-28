f17895c2a70a62050e66b5ece483c33d0e7c0c7b
veeroe <>
2014-10-28 11:11:50 +0100

    test

---
+++ employee/src/employee/App.java
@@ -0,0 +1,12 @@
+package employee;
+
+public class App {
+
+	public static void main(String[] args) {
+		
+		Employee emp1 = new Employee();
+		
+		emp1.printFullName("Peter", "Müller");
+
+	}
+}
---
+++ employee/src/employee/Employee.java
@@ -0,0 +1,13 @@
+package employee;
+
+public class Employee {
+	
+	String firstName;
+	String lastName;
+	Integer ID;
+
+	void printFullName(String first, String last) {
+		
+		System.out.println(first + " " + last);
+	}
+}
